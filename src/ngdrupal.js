b5999f150c33928e6694cb143d506b6c14922cb8
marthinal <jm.rodriguez.velez@gmail.com>
2015-01-29 21:33:33 +0100

    remove login function

--- src/ngdrupal.js
+++ src/ngdrupal.js
@@ -11,15 +11,7 @@ var module = angular.module('ngdrupal', ['restangular'])
   // Login Service.
   .factory('loginService', function($http, Restangular) {
     return {
-      login:function(user) {
-        data = '&name=' + user.name + '&pass=' + user.pass + '&form_id=user_login_form';
-        var login = Restangular.one('user/login').customPOST(
-          data,
-          undefined, // put your path here
-          undefined, // params here, e.g. {format: "json"}
-          {'Content-Type': "application/x-www-form-urlencoded; charset=UTF-8"}
-        );
-      }
+
     }
   })
 
