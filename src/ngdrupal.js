ef4c3db6f32cbcd6e3e8b60c9eef225cb681fec4
marthinal <jm.rodriguez.velez@gmail.com>
2015-02-07 15:00:36 +0100

    loginService Post request done.

--- src/ngdrupal.js
+++ src/ngdrupal.js
@@ -1,18 +1,28 @@
-
 (function() {
 
-var module = angular.module('ngdrupal', ['restangular'])
+  var module = angular.module('ngdrupal', ['restangular'])
+    // Set the Base url.
+    .config(function(RestangularProvider) {
+      // Base Url.
+      RestangularProvider.setBaseUrl('http://MySiteSuperPoweredByDrupal/');
+      RestangularProvider.setDefaultHttpFields({withCredentials: true});
+    })
 
-  // Set the Base url.
-  .config(function(RestangularProvider) {
-    RestangularProvider.setBaseUrl('http://example.com/');
-  })
-
-  // Login Service.
-  .factory('loginService', function($http, Restangular) {
-    return {
-
-    }
-  })
+    // Login Service.
+    // Apply https://www.drupal.org/node/2403307
+    // and https://www.drupal.org/node/2419825
+    .factory('loginService', function($http, Restangular) {
+      return {
+        login:function(user) {
+          return Restangular.one('user_login').customPOST(
+            JSON.stringify({"op": "login","credentials": {"name":user.name,"pass":user.pass}}),
+            undefined, // put your path here
+            undefined, // params here, e.g. {format: "json"}
+            {ContentType: 'application/json', Accept: 'application/json'}
+          );
+        }
+      }
+    })
 
 })();
+
