435c7164e9a38252634f44d57305a31e49006155
marthinal <jm.rodriguez.velez@gmail.com>
2015-02-13 12:43:09 +0100

    Unit Test for Register Service

--- spec/ngDrupalSpec.js
+++ spec/ngDrupalSpec.js
@@ -1,7 +1,6 @@
 describe('ngDrupal Unit Tests:', function() {
 
   var httpBackend, Restangular, q, scope;
-
   beforeEach(module('ngdrupal'));
   // then we use the $injector to obtain the instances of the services we would like to mock/use
   // but not of the service that we want to test
@@ -12,7 +11,8 @@ describe('ngDrupal Unit Tests:', function() {
     scope = $rootScope.$new();
   }));
 
-  it('Unit Test for logoutService', inject(function(loginService) {
+  // LOGIN.
+  it('Unit Test for loginService', inject(function(loginService) {
     // set up a spy on Restangular, so we test with what parameters it was called, also allow the call to continue
     spyOn(Restangular, 'one').and.callThrough();
     // a parameter with which the http service we expect to be called
@@ -28,7 +28,7 @@ describe('ngDrupal Unit Tests:', function() {
     httpBackend.flush();
   }));
 
-
+  // LOGOUT.
   it('Unit Test for logoutService', inject(function($rootScope, $q, logoutService, getTokenService) {
     deferred = $q.defer();
     // set up a spy on Restangular, so we test with what parameters it was called, also allow the call to continue
@@ -46,6 +46,23 @@ describe('ngDrupal Unit Tests:', function() {
     $rootScope.$digest();
     // handle restangular expectations
     expect(Restangular.one).toHaveBeenCalledWith('user_login');
+    // flush the backend to unproxy the restangular promise
+    httpBackend.flush();
+  }));
+
+  // REGISTER.
+  it('Unit Test for registerService', inject(function(registerService) {
+    // set up a spy on Restangular, so we test with what parameters it was called, also allow the call to continue
+    spyOn(Restangular, 'one').and.callThrough();
+    // a parameter with which the http service we expect to be called
+    var user = {name:"marthinal", email:"marthinal@drupalisawesome.com"};
+    // httpBackend would append a "/" in front of a restangular call
+    httpBackend.expectPOST('http://MySiteSuperPoweredByDrupal/entity/user/register', {"name":[{"value": "marthinal"}],"mail":[{"value": "marthinal@drupalisawesome.com"}]})
+      .respond();
+    // now call our service
+    registerService.register(user);
+    // handle restangular expectations
+    expect(Restangular.one).toHaveBeenCalledWith('entity/user/register');
     // flush the backend to unproxy the restangular promise
     httpBackend.flush();
   }));
