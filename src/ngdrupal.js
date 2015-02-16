07eeaf97d92e8fbedf793e00298951601e04d807
marthinal <jm.rodriguez.velez@gmail.com>
2015-02-16 21:04:55 +0100

    create and remove session

--- src/ngdrupal.js
+++ src/ngdrupal.js
@@ -8,7 +8,8 @@
       RestangularProvider.setDefaultHttpFields({withCredentials: true});
     })
 
-    .service('Session', function () {
+    // Remove this Service if you want to use your own session from your app.
+    .service('DrupalSession', function () {
       // TODO TBD session fields.
       this.create = function (userId, userName, userLangcode) {
         this.userId = userId;
@@ -26,7 +27,7 @@
     // AUTH SERVICE.
     // Apply https://www.drupal.org/node/2403307
     // and https://www.drupal.org/node/2419825
-    .factory('AuthService', function(Restangular, Session) {
+    .factory('AuthService', function(Restangular, DrupalSession) {
       return {
         login:function(user) {
           return Restangular.one('user_login').customPOST(
@@ -34,14 +35,19 @@
             undefined, // put your path here
             undefined, // params here, e.g. {format: "json"}
             {ContentType: 'application/json', Accept: 'application/json'}
+            // Remove if you want to use your own session from your app.
           ).then(function(result) {
-              Session.create(result.uid[0].value, result.name[0].value, result.langcode[0].value);
+              DrupalSession.create(result.uid[0].value, result.name[0].value, result.langcode[0].value);
             });
         },
 
         logout:function() {
           return this.getToken()
             .then(function(token) {
+
+              // Remove if you want to use your own session from your app.
+              DrupalSession.destroy();
+
               return Restangular.one('user_login').customPOST(
                 JSON.stringify({"op": "logout"}),
                 undefined, // put your path here
@@ -52,13 +58,18 @@
           );
         },
 
+        isAuthenticated:function() {
+          // Use your own session here if you are not using the DrupalSession Service.
+          return !!DrupalSession.userId;
+        },
+
         getToken:function() {
           return Restangular.one('rest/session/token').get();
         }
       }
     })
 
-    // REGISTER SERVICE.
+    // REGISTER SERVICE. Apply https://www.drupal.org/node/2291055
     .factory('RegisterService', function(Restangular) {
       return {
         register:function(user) {
