3a2eb2e0d0cad7c357b94738b66b52e9504ecb23
marthinal <marthinal@387593.no-reply.drupal.org>
2016-03-17 20:56:17 +0100

    Update doc. Authservice

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # ngDrupal
-Angular module for Drupal 8 Rest Services.
+
+Angular module for Drupal 8 Rest Services using cookie sessions. Actually this
+ is a WIP.
+
+ This module uses Restangular(https://github.com/mgonto/restangular#restangular)
+  module as a dependency.
+
+
+List of available services:
+
+AUTHSERVICE:
+
+ ```
+  // Login for an ionic(http://ionicframework.com/) app
+  // user.name = 'marthinal'
+  // user.pass = 'supersecretpass'
+  $scope.login = function(user) {
+    AuthService.login(user)
+      // Go to "My account".
+      .then(function(data) {
+        $state.go('tab.account');
+      })
+      .catch(function() {
+        $ionicPopup.alert({
+          title: $translate.instant('login'),
+          template: $translate.instant('unrecognized_username_password')
+        });
+    });
+  };
+
+  // Logout for an ionic(http://ionicframework.com/) app.
+  $scope.logout = function() {
+    AuthService.logout()
+      // Go to "Login tab".
+      .then(function() {
+        $state.go('login');
+      })
+      .catch(function() {
+        $ionicPopup.alert({
+          title: 'Logout',
+          template: 'Oops Cannot logout!'
+        });
+      });
+    }
+
+    // GetToken
+    AuthService.getToken()
+      .then(function(token) {
+        // Operations.
+      });
+
+ ```
--- src/ngdrupal.js
+++ src/ngdrupal.js
@@ -1,62 +1,62 @@
 (function() {
 
   var module = angular.module('ngdrupal', ['restangular'])
-    // Set the Base url.
     .config(function(RestangularProvider) {
       RestangularProvider.setDefaultHttpFields({withCredentials: true});
     })
 
     .service('DrupalSession', function () {
-      // TODO TBD session fields.
       this.create = function (user) {
         this.user = user.user;
+
         this.sessionName = user.sessionName;
         this.sessId = user.sessId;
+        this.extra = user.extra;
       };
       this.destroy = function () {
         this.user = null;
         this.sessionName = null;
         this.sessId = null;
+        this.extra = null;
       };
       return this;
     })
 
     // AUTH SERVICE.
-    // Apply https://www.drupal.org/node/2403307
+    // @See https://www.drupal.org/node/2403307
     // and https://www.drupal.org/node/2419825
     .factory('AuthService', function(Restangular, DrupalSession) {
       return {
-        login:function(user) {
+        login: function(user) {
           return Restangular.one('user_login').customPOST(
             JSON.stringify({"op": "login","credentials": {"name":user.name,"pass":user.pass}}),
-            undefined, // put your path here
-            undefined, // params here, e.g. {format: "json"}
+            undefined, // Path
+            undefined, // Params {format: "json"}
             {ContentType: 'application/json', Accept: 'application/json'}
-            // Remove if you want to use your own session from your app.
           ).then(function(result) {
-              DrupalSession.create(result);
-            });
+            DrupalSession.create(result);
+          });
         },
 
-        logout:function() {
+        logout: function() {
           return this.getToken()
             .then(function(token) {
-              DrupalSession.destroy();
-              return Restangular.one('user_login').customPOST(
-                JSON.stringify({"op": "logout"}),
-                undefined, // put your path here
-                undefined, // params here, e.g. {format: "json"}
-                {ContentType: 'application/json', Accept: 'application/json', 'X-CSRF-Token': token}
-              );
-            }
-          );
+                DrupalSession.destroy();
+                return Restangular.one('user_login').customPOST(
+                  JSON.stringify({"op": "logout"}),
+                  undefined, // Path
+                  undefined, // Params {format: "json"}
+                  {ContentType: 'application/json', Accept: 'application/json', 'X-CSRF-Token': token}
+                );
+              }
+            );
         },
 
-        isAuthenticated:function() {
+        isAuthenticated: function() {
           return DrupalSession.user && !!DrupalSession.user.uid[0].value;
         },
 
-        getToken:function() {
+        getToken: function() {
           return Restangular.one('rest/session/token').get();
         }
       }
@@ -65,7 +65,7 @@
     // REGISTER SERVICE. Apply https://www.drupal.org/node/2291055
     .factory('RegisterService', function(Restangular) {
       return {
-        register:function(user) {
+        register: function(user) {
           return Restangular.one('entity/user/register').customPOST(
             JSON.stringify({"name":[{"value": user.name}],"mail":[{"value": user.email}]}),
             undefined, // put your path here
