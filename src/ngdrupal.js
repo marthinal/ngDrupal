a957a73d513e1b0adb7d751dc8998f6b26419123
marthinal <jm.rodriguez.velez@gmail.com>
2016-04-22 19:00:59 +0200

    Obtain the csrf-token from DrupalSession

--- src/ngdrupal.js
+++ src/ngdrupal.js
@@ -8,16 +8,17 @@
     .service('DrupalSession', function () {
       this.create = function (user) {
         this.user = user.user;
-
         this.sessionName = user.sessionName;
         this.sessId = user.sessId;
         this.extra = user.extra;
+        this.csrfToken = user.csrfToken;
       };
       this.destroy = function () {
         this.user = null;
         this.sessionName = null;
         this.sessId = null;
         this.extra = null;
+        this.csrfToken = null;
       };
       return this;
     })
@@ -39,17 +40,13 @@
         },
 
         logout: function() {
-          return this.getToken()
-            .then(function(token) {
-                DrupalSession.destroy();
-                return Restangular.one('user_login').customPOST(
-                  JSON.stringify({"op": "logout"}),
-                  undefined, // Path
-                  undefined, // Params {format: "json"}
-                  {ContentType: 'application/json', Accept: 'application/json', 'X-CSRF-Token': token}
-                );
-              }
-            );
+          DrupalSession.destroy();
+          return Restangular.one('user_login').customPOST(
+            JSON.stringify({"op": "logout"}),
+            undefined, // Path
+            undefined, // Params {format: "json"}
+            {ContentType: 'application/json', Accept: 'application/json', 'X-CSRF-Token': DrupalSession.csrfToken}
+          );
         },
 
         isAuthenticated: function() {
@@ -63,68 +60,62 @@
     })
 
     // ENTITY SERVICE for CRUD operations.
-    .factory('EntityService', function(myConfig, Restangular, AuthService, $q) {
+    .factory('EntityService', function(myConfig, Restangular, AuthService, $q, DrupalSession) {
       return {
         createMultiple: function(entityType, bundle, values) {
-          return AuthService.getToken()
-            .then(function(token) {
-              var promises = [];
-              angular.forEach(values, function(data) {
-                // Building the json.
-                var request = {};
-                request = {"_links": {"type": {"href": myConfig.url + '/rest/type/' + entityType + '/' + bundle}}};
-                // Adds values per field.
-                angular.forEach(data, function(value, key) {
-                  if (value !== null && typeof value !== "object") {
-                    request[key] = [{"value": value}];
-                  }
-                  else if(typeof value === "object") {
-                    request[key] = value;
-                  }
-                });
-                var promise = Restangular.one('entity/' + entityType).customPOST(
-                  JSON.stringify(request),
-                  undefined, // Path
-                  undefined, // Params {format: "json"}
-                  {"Content-Type": 'application/hal+json', Accept: 'application/hal+json', 'X-CSRF-Token': token}
-                );
-                promises.push(promise);
-              });
-              return $q.all(promises);
+          var promises = [];
+          angular.forEach(values, function(data) {
+            // Building the json.
+            var request = {};
+            request = {"_links": {"type": {"href": myConfig.url + '/rest/type/' + entityType + '/' + bundle}}};
+            // Adds values per field.
+            angular.forEach(data, function(value, key) {
+              if (value !== null && typeof value !== "object") {
+                request[key] = [{"value": value}];
+              }
+              else if(typeof value === "object") {
+                request[key] = value;
+              }
             });
+            var promise = Restangular.one('entity/' + entityType).customPOST(
+              JSON.stringify(request),
+              undefined, // Path
+              undefined, // Params {format: "json"}
+              {"Content-Type": 'application/hal+json', Accept: 'application/hal+json', 'X-CSRF-Token': DrupalSession.csrfToken}
+            );
+            promises.push(promise);
+          });
+          return $q.all(promises);
         },
+
         create: function(entityType, bundle, values) {
-          return AuthService.getToken()
-            .then(function(token) {
-              // Building the json.
-              var request = {};
-              request = {"_links": {"type": {"href": myConfig.url + '/rest/type/' + entityType + '/' + bundle}}};
-              // Add values per field to the json.
-              angular.forEach(values, function(value, key) {
-                if (value !== null && typeof value !== "object") {
-                  request[key] = [{"value": value}];
-                }
-                else if(typeof value === "object") {
-                  request[key] = value;
-                }
-              });
-              return Restangular.one('entity/' + entityType).customPOST(
-                JSON.stringify(request),
-                undefined, // Path
-                undefined, // Params {format: "json"}
-                {"Content-Type": 'application/hal+json', Accept: 'application/hal+json', 'X-CSRF-Token': token}
-              );
-            });
+          var request = {};
+          request = {"_links": {"type": {"href": myConfig.url + '/rest/type/' + entityType + '/' + bundle}}};
+
+          // Add values per field to the json.
+          angular.forEach(values, function(value, key) {
+            if (value !== null && typeof value !== "object") {
+              request[key] = [{"value": value}];
+            }
+            else if(typeof value === "object") {
+              request[key] = value;
+            }
+          });
+
+          return Restangular.one('entity/' + entityType).customPOST(
+            JSON.stringify(request),
+            undefined, // Path
+            undefined, // Params {format: "json"}
+            {"Content-Type": 'application/hal+json', Accept: 'application/hal+json', 'X-CSRF-Token': DrupalSession.csrfToken}
+          );
         },
+
         delete: function(entityType, id) {
-          return AuthService.getToken()
-            .then(function(token) {
-              return Restangular.one( entityType + '/' + id).customDELETE(
-                undefined, // Path
-                undefined, // Params {format: "json"}
-                {"Content-Type": 'application/hal+json', Accept: 'application/hal+json', 'X-CSRF-Token': token}
-              );
-            });
+          return Restangular.one( entityType + '/' + id).customDELETE(
+            undefined, // Path
+            undefined, // Params {format: "json"}
+            {"Content-Type": 'application/hal+json', Accept: 'application/hal+json', 'X-CSRF-Token': DrupalSession.csrfToken}
+          );
         }
       };
     })
